3a976673e3dc4ad42be831d3a686b07682c81e9d
KJ-Hyeon <bark052526@gmail.com>
2023-01-06 16:35:05 +0900

    스플래시 화면 구현

---
+++ app/src/main/java/com/jeong/android/android_shoppi/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.jeong.android.android_shoppi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 스플래시 화면은 layout을 통해서 만들지 않고 drawable을 통해서 구현 -> API30이하 버전에서 실행
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
