f42c58ee3e1d1d3683bc77c9e3112bd2763e4677
KJ-Hyeon <bark052526@gmail.com>
2023-01-09 17:23:59 +0900

    BottomNavigation을 통한 화면 이동

--- app/src/main/java/com/jeong/android/android_shoppi/MainActivity.kt
+++ app/src/main/java/com/jeong/android/android_shoppi/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jeong.android.android_shoppi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         // bottomNavigation의 컬러 참조를 초기화
         bottomNavigationView.itemIconTintList = null
+        // bottomNavigatonView와 FragmentContainerView의 NavHostFragment를 연결
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
