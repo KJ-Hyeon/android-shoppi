41407de68b15218a621938e6101f5aec95b73665
KJ-Hyeon <bark052526@gmail.com>
2023-01-09 16:34:59 +0900

    BottomNavigation 추가

--- app/src/main/java/com/jeong/android/android_shoppi/MainActivity.kt
+++ app/src/main/java/com/jeong/android/android_shoppi/MainActivity.kt
@@ -2,10 +2,15 @@ package com.jeong.android.android_shoppi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        // bottomNavigation의 컬러 참조를 초기화
+        bottomNavigationView.itemIconTintList = null
     }
 }
