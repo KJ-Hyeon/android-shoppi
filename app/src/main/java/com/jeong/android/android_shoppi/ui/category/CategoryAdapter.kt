cc1b7966fceb1fdbcb08ec85ccc2c03952efac16
KJ-Hyeon <bark052526@gmail.com>
2023-01-13 16:55:28 +0900

    카테고리 목록 Adapter 및 databinding 적용

---
+++ app/src/main/java/com/jeong/android/android_shoppi/ui/category/CategoryAdapter.kt
@@ -0,0 +1,41 @@
+package com.jeong.android.android_shoppi.ui.category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.jeong.android.android_shoppi.databinding.ItemCategoryBinding
+import com.jeong.android.android_shoppi.model.Category
+
+class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class CategoryViewHolder(private val binding : ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(category: Category) {
+            binding.category = category
+            // binding된 data가 바로 view에 반영
+            binding.executePendingBindings()
+        }
+    }
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.categoryId == newItem.categoryId
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+
+}
